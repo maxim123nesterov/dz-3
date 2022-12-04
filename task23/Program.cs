45abbcae4a82e683ea18bd80ac60d5cdcba1c102
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-05 01:13:19 +0300

    задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int Y = 3;
+for (int P=1;P<=A;P++)
+{;
+   double root = Math.Pow(P, Y);
+   Console.Write($" {root}");
+}
